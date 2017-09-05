5ffacefb659bfebb9ff2f903f112f73d4986ae7b
Jessica Mott <wigmore3.14@gmail.com>
2017-09-05 18:57:41 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+# DISPLAY BOARD
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,10 +7,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+# USER INPUT
 def input_to_index(input)
-  
+
 end
 
+# USER MOVE
 def move(board, position, character = "x")
 
 end
